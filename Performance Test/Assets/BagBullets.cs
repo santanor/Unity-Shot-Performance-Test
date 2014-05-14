3e1cf3e243ff9a8a8e180c7a309efecee3912640
Menti <josementi@gmail.com>
2014-05-14 02:09:49 +0200

    Test 'completed' I forgot to save the slow case, I'm gonna add the two cases and develop a Standalone to compare the two algorithm

---
+++ Performance Test/Assets/BagBullets.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+using System.Collections;
+
+public class BagBullets : MonoBehaviour {
+
+
+	GameObject bullet;
+	GameObject clonedBullet;
+	Queue<GameObject> bullets;
+	// Use this for initialization
+	void Start () {
+		bullets = new Queue<GameObject>();
+		bullet = (GameObject)Resources.Load("Bullet");
+		for(int i = 0; i < 200; i++)
+		{
+			clonedBullet = (GameObject)Instantiate(bullet, new Vector3(-100,-100,-100), Quaternion.identity);
+			clonedBullet.SetActive(false);
+			bullets.Enqueue(clonedBullet);
+		}
+	}
+	
+	public GameObject GetBullet()
+	{
+		if(bullets.Count > 0)
+			return bullets.Dequeue();
+
+		if(bullets.Count < 50)
+			StartCoroutine(Reload());
+		return GetBullet();
+	}
+
+	public IEnumerator Reload()
+	{
+		for(int i = 0; i < 50; i++)
+		{
+			clonedBullet = (GameObject)Instantiate(bullet, new Vector3(-100,-100,-100), Quaternion.identity);
+			clonedBullet.SetActive(false);
+			bullets.Enqueue(clonedBullet);
+		}
+		yield return null;
+	}
+
+	public void ReturnBullet(GameObject go, float seconds)
+	{
+		StartCoroutine(Return (go, seconds));
+	}
+
+	IEnumerator Return(GameObject go, float seconds)
+	{
+		yield return new WaitForSeconds(seconds);
+		go.transform.position = new Vector3(-100,-100,-100);
+		go.SetActive(false);
+		bullets.Enqueue(go);
+	}
+}
---
+++ Performance Test/Assets/PlaceTanks.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceTanks : MonoBehaviour {
+
+	float currentFilePos;
+	int counter;
+	float currentColumnPos;
+	// Use this for initialization
+	void Start () {
+		currentFilePos = 15f;
+		counter = 0;
+		currentColumnPos = 120f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyUp(KeyCode.Space))
+		{
+
+			GameObject go = (GameObject)Resources.Load("Tank");
+			Instantiate(go, new Vector3(currentColumnPos, 0, currentFilePos), Quaternion.identity);
+			currentColumnPos += 3f;
+			counter ++;
+			if (counter >= 10)
+			{
+				currentColumnPos = 120f;
+				currentFilePos += 5f;
+				counter = 0;
+			}
+
+
+		}
+	}
+}
